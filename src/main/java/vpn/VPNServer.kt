7edac6887e6a85988c2849c1ba8d5c13406c9a06
rmanasipov@beeline.ru <rmanasipov@beeline.ru>
2023-08-16 00:28:41 +0300

    Dzen-tr2

--- src/main/java/vpn/VPNServer.kt
+++ src/main/java/vpn/VPNServer.kt
@@ -1,18 +1,11 @@
 package vpn
 
 import java.net.ServerSocket
-import java.net.Socket
 
 
 fun main(args: Array<String>) {
     val serverSocket = ServerSocket(12345)
 
 
-        val clientSocket = serverSocket.accept()
-        print(clientSocket.receiveBufferSize)
-        print("Got connection from client..")
-
-    val socket = Socket("127.0.0.1", 12345)
-    socket.getOutputStream().write(12)
 }
 
