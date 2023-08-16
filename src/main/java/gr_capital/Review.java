471b82d7fc656313d9262ac5f73b02d3131b54e1
rmanasipov@beeline.ru <rmanasipov@beeline.ru>
2023-08-16 22:38:43 +0300

    Собес из gr_capital (рефакторинг)

--- src/main/java/gr_capital/Review.java
+++ src/main/java/gr_capital/Review.java
@@ -2,22 +2,32 @@ package gr_capital;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Review {
     public static void main(String[] args) {
-//        String str1 = "String1";
-//        String str2 = "String" + 1;
-//        String str3 = "String1";
-//
-//        System.out.println(str1 == str2);
-//        System.out.println(str1 == (getString() + 1));
-//        System.out.println(str1 == str3);
+
+        List<String> strings = List.of("AB", "BA", "EC");
+
+        strings.stream()
+                .sorted(Comparator.comparing(s -> s.substring(1)))
+                .forEach(s -> System.out.println("String " + s));
 
         Int int1 = Int.valueOf(125);
         Int int2 = Int.valueOf(-1);
         Int int3 = Int.valueOf(257);
+    }
+
+    static void aboutStrings() {
+        String str1 = "String1";
+        String str2 = "String" + 1;
+        String str3 = "String1";
+
+        System.out.println(str1 == str2);
+        System.out.println(str1 == (getString() + 1));
+        System.out.println(str1 == str3);
     }
 
     static String getString() {
